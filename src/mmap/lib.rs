68b3f2de02f1154ae6100956842822c8c800dfa9
Richo Healey <richo@psych0tik.net>
2014-05-09 22:24:08 -0700

    Update to be compatible with rust master

--- src/mmap/lib.rs
+++ src/mmap/lib.rs
@@ -1,7 +1,7 @@
 #[desc = "Rust mmap wrapper"]
 #[license = "MIT"]
 
-use std::libc;
+extern crate libc;
 use std::os;
 use std::fmt;
 use std::vec::raw;
@@ -11,8 +11,8 @@ struct MappedRegion {
     len : libc::size_t
 }
 
-impl fmt::Default for MappedRegion {
-    fn fmt(value: &MappedRegion, f: &mut std::fmt::Formatter) {
+impl fmt::Show for MappedRegion {
+    fn show(value: &MappedRegion, f: &mut std::fmt::Formatter) {
         write!(f.buf, "MappedRegion\\{{}, {}\\}", value.addr, value.len);
     }
 }
@@ -20,7 +20,6 @@ impl fmt::Default for MappedRegion {
 impl Drop for MappedRegion {
     #[fixed_stack_segment]
     fn drop(&mut self) {
-        debug!(format!("munmapping {}", self.addr));
         unsafe {
             if libc::munmap(self.addr, self.len) < 0 {
                 fail!(format!("munmap({}, {}): {}", self.addr, self.len, os::last_os_error()));
@@ -46,7 +45,7 @@ fn mmap(size: libc::size_t) -> Result<MappedRegion, ~str> {
 
 pub fn mmap_as_slice<T, U>(size: libc::size_t, f: &fn(v: &[T]) -> U) -> U {
     match mmap(size) {
-        Ok(r) => { unsafe { raw::buf_as_slice(r.addr as *T, r.len as uint, f) } }
+        Ok(r) => { unsafe { raw::from_buf(r.addr as *T, r.len as uint, f) } }
         Err(s) => { fail!(s) }
     }
 }
@@ -58,8 +57,8 @@ fn test_all() {
         Err(s) => { println!("err: {}", s); }
     }
 
-    do mmap_as_slice(4096) |v : &[u8]| {
+     mmap_as_slice(4096, |v : &[u8]| {
         println!("v[0] = {}", v[0]);
-    }
+    });
     println("exiting");
 }
